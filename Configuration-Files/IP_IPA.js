7951b41ce028b76361dcafada9d7a8301b03d468
Allen <144861909+SHanQ7@users.noreply.github.com>
2024-09-11 12:45:59 +0800

    Update IP_IPA.js

--- Configuration-Files/IP_IPA.js
+++ Configuration-Files/IP_IPA.js
@@ -4,28 +4,34 @@ if ($response.statusCode != 200) {
 
 const emojis= ['🆘','🈲','⚠️','🔞','📵','🚦','🏖','🖥','📺','🐧','🐬','🦉','🍄','⛳️','🚴','🤑','👽','🤖','🎃', '👺', '👁', '🐶', '🐼','🐌', '👥']
 var city0 = "高谭市";
-var isp0 = "limbopro.com";
+var isp0 = "Cross-GFW.org";
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 function City_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return city0
-//emojis[getRandomInt(emojis.length)]
+  if (para) {
+    return para;
+  } else {
+    return city0;
+    //emojis[getRandomInt(emojis.length)]
   }
 }
 
 function ISP_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return isp0
-//emojis[getRandomInt(emojis.length)]
+  if (para) {
+    return para;
+  } else {
+    return isp0;
+    //emojis[getRandomInt(emojis.length)]
+  }
+}
+
+function Area_check(para) {
+  if (para == "中华民国") {
+    return "台湾";
+  } else {
+    return para;
   }
 }
 
@@ -33,8 +39,8 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = flags.get(obj['countryCode']) + ' '+ obj['country']; //+Area_check(obj['country']);
-var subtitle = City_ValidCheck(obj['city']) + '-' + '(' + ISP_ValidCheck(obj['org']) + ')';
+var title = flags.get(obj["countryCode"]) + " " + City_ValidCheck(obj["city"]); //+Area_check(obj['country']);
+var subtitle = City_ValidCheck(obj['city']) + '-' + ISP_ValidCheck(obj['org']);
 var ip = obj["query"];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 
