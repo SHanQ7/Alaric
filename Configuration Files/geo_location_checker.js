8df61b60d88c2496a5248882d95623e26500285b
Allen <144861909+SHanQ7@users.noreply.github.com>
2024-09-11 12:14:35 +0800

    Update geo_location_checker.js

--- Configuration Files/geo_location_checker.js
+++ Configuration Files/geo_location_checker.js
@@ -40,8 +40,8 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = flags.get(obj["countryCode"]) + " " + Area_check(obj['country']);
-var subtitle = City_ValidCheck(obj['city'])+'-'+'('+ ISP_ValidCheck(obj['org'])+')';
+var title = flags.get(obj["countryCode"]); //+Area_check(obj['country']);
+var subtitle = City_ValidCheck(obj['city']) + '-' + '(' + ISP_ValidCheck(obj['org']) + ')';
 var ip = obj["query"];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 
