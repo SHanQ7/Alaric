8ee79e3ca45796d42bec87ef101ae7c4da91952f
Allen <144861909+SHanQ7@users.noreply.github.com>
2025-05-10 02:06:12 +0800

    Update ip-api.js

--- script/ip-api.js
+++ script/ip-api.js
@@ -55,17 +55,23 @@ let title = flags.get(obj['countryCode']) + ' ' + country + ' ' + region;
 let subtitle = (displayCity ? displayCity + ' · ' : '') + obj['query'] + ' · ' + ISP_ValidCheck(obj['isp']);
 
 // 长按节点选择“查看节点信息”时的信息
-let description = 
-  '--------------------------------------\n' +
-  '国家      : ' + countryCode + ' ' + country + '\n' +
-  '地区      : ' + obj['region'] + ' ' + region + '\n' +
-  '城市      : ' + obj['city'] + '\n' +
-  'IP地址    : ' + ip + '\n' +
-  '运营商    : ' + isp + '\n' +
-  '经纬度    : ' + lat + ' / ' + lon + '\n' +
-  '时区      : ' + timezone + '\n' +
-  '--------------------------------------';
+let description = `
+--------------------------------------
+${countryCode} ${country}
 
+${obj['region']} ${region}
+
+${obj['city']}
+
+${ip}
+
+${isp}
+
+${lat} / ${lon}
+
+${timezone}
+--------------------------------------
+`;
 $done({title, subtitle, ip, description});
 
 // 国家映射表
