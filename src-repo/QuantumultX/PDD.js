f12be8973dbfb77e91e7b389b55e8e1740e99773
Allen <144861909+SHanQ7@users.noreply.github.com>
2025-11-02 13:39:45 +0800

    Update PDD.js

--- src-repo/QuantumultX/PDD.js
+++ src-repo/QuantumultX/PDD.js
@@ -3,50 +3,6 @@
 #!update = 2025-09-13
 #!版本号：7.55.0
 
-//pdd
-# === IP-CIDR 规则 ===
-IP-CIDR, 36.152.46.3/32, REJECT
-IP-CIDR, 36.152.46.13/32, REJECT
-IP-CIDR, 36.152.46.15/32, REJECT
-IP-CIDR, 49.235.102.190/32, REJECT
-IP-CIDR, 81.69.104.17/32, REJECT
-IP-CIDR, 81.69.116.33/32, REJECT
-IP-CIDR, 81.69.130.131/32, REJECT
-IP-CIDR, 101.35.204.35/32, REJECT
-IP-CIDR, 101.35.212.35/32, REJECT
-IP-CIDR, 114.110.96.1/32, REJECT
-IP-CIDR, 114.110.96.6/32, REJECT
-IP-CIDR, 114.110.96.26/32, REJECT
-IP-CIDR, 114.110.97.30/32, REJECT
-IP-CIDR, 114.110.97.97/32, REJECT
-IP-CIDR, 114.110.98.2/32, REJECT
-IP-CIDR, 114.110.98.3/32, REJECT
-IP-CIDR, 117.185.242.18/32, REJECT
-IP-CIDR, 117.185.242.187/32, REJECT
-IP-CIDR, 117.185.244.145/32, REJECT
-IP-CIDR, 121.5.84.85/32, REJECT
-IP-CIDR, 183.194.214.14/32, REJECT
-IP-CIDR, 183.194.214.20/32, REJECT
-IP-CIDR, 183.194.214.21/32, REJECT
-
-# === DOMAIN 规则 ===
-DOMAIN, apm-a.pinduoduo.com, REJECT
-DOMAIN, apm.pinduoduo.com, REJECT
-DOMAIN, cd-1.pddpic.com, REJECT
-DOMAIN, cdl-1.pddpic.com, REJECT
-DOMAIN, cdl-p2.pddpic.com, REJECT
-DOMAIN, file.pinduoduo.com, REJECT
-DOMAIN, m.pinduoduo.net, REJECT
-DOMAIN, meta.pinduoduo.com, REJECT
-DOMAIN, sdk.1rtb.net, REJECT
-DOMAIN, ta-a.pinduoduo.com, REJECT
-DOMAIN, ta.pinduoduo.com, REJECT
-DOMAIN, th-a.pinduoduo.com, REJECT
-DOMAIN, th-b.pinduoduo.com, REJECT
-DOMAIN, th.pinduoduo.com, REJECT
-DOMAIN, titan.pinduoduo.com, REJECT
-DOMAIN, xg.pinduoduo.com, REJECT
-
 ^https:\/\/api\.pinduoduo\.com\/api\/alexa\/goods\/back_up url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/alexa\/homepage\/hub url jsonjq-response-body 'del(.result.icon_set, .result.search_bar_hot_query, .result.dy_module.irregular_banner_dy, .result.dy_module.recommend_fresh_info) | if .result.bottom_tabs then .result.bottom_tabs |= map(select(.link == "index.html" or .link == "chat_list.html" or .link == "personal.html")) else . end'
 ^https:\/\/api\.pinduoduo\.com\/api\/aquarius\/hungary\/global\/homepage\? url reject-dict
