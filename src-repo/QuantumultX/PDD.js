467bf3ec923d5be25faa02837a415b31b7db4d4e
Allen <144861909+SHanQ7@users.noreply.github.com>
2025-11-02 13:48:21 +0800

    Update PDD.js

--- src-repo/QuantumultX/PDD.js
+++ src-repo/QuantumultX/PDD.js
@@ -3,7 +3,6 @@
 #!update = 2025-11-01
 #!版本号：7.79.0
 
-//pdd
 # === IP-CIDR 规则 ===
 IP-CIDR, 36.152.46.3/32, REJECT
 IP-CIDR, 36.152.46.13/32, REJECT
@@ -48,10 +47,11 @@ DOMAIN, titan.pinduoduo.com, REJECT
 DOMAIN, xg.pinduoduo.com, REJECT
   
 ^https:\/\/api\.pinduoduo\.com\/api\/engels\/wait\/receive\/review url reject-dict
-^https:\/\/api\.pinduoduo\.com\/search url jsonjq-response-body 'del(.expansion)'
-^https:\/\/api\.pinduoduo\.com\/api\/philo\/personal\/hub url jsonjq-response-body 'del(.monthly_card_entrance, .personal_center_style_v2_vo, .icon_set.icons, .icon_set.top_personal_icons)'
-^https:\/\/api\.pinduoduo\.com\/api\/oak\/integration\/render url jsonjq-response-body 'del(.bottom_section_list, .ui.bottom_section, .ui.live_section.float_info)'
-^https:\/\/api\.pinduoduo\.com\/api\/caterham\/v3\/query\/order_detail_group url jsonjq-response-body 'del(.data.goods_list)'
+^https:\/\/api\.pinduoduo\.com\/api\/alexa\/homepage\/hub url json-response-body 'del(.result.icon_set, .result.search_bar_hot_query, .result.dy_module.irregular_banner_dy,.result.dy_module.recommend_fresh_info) | if .result.bottom_tabs then .result.bottom_tabs |= map(select(.link == "index.html" or .link == "chat_list.html" or .link == "personal.html")) else . end'
+^https:\/\/api\.pinduoduo\.com\/search url json-response-body 'del(.expansion)'
+^https:\/\/api\.pinduoduo\.com\/api\/philo\/personal\/hub url json-response-body 'del(.monthly_card_entrance, .personal_center_style_v2_vo, .icon_set.icons, .icon_set.top_personal_icons)'
+^https:\/\/api\.pinduoduo\.com\/api\/oak\/integration\/render url json-response-body 'del(.bottom_section_list, .ui.bottom_section, .ui.live_section.float_info)'
+^https:\/\/api\.pinduoduo\.com\/api\/caterham\/v3\/query\/order_detail_group url json-response-body 'del(.data.goods_list)'
 ^https:\/\/api\.pinduoduo\.com\/api\/aristotle\/unrated_order_for_unreceived_tab url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/aristotle\/query_order_list_tabs_element url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/aquarius\/hungary\/global\/homepage\? url reject-dict
@@ -65,7 +65,6 @@ DOMAIN, xg.pinduoduo.com, REJECT
 ^https:\/\/api\.pinduoduo\.com\/api\/engels\/reviews\/require\/append url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/caterham\/v3\/query\/my_order_group url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/caterham\/v3\/query\/likes url reject-dict
-
 ^https:\/\/api\.pinduoduo\.com\/api\/phantom\/gbdbpdv\/extra url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/manufacturer\/cross\/shortcut\/list url reject-dict
 ^https:\/\/api\.pinduoduo\.com\/api\/dunkirk\/liveactivity\/push\/create\/url\/report url reject-dict
