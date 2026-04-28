53120ab8ed53b9d0defb152213cb3342961d5bb7
Allen <144861909+SHanQ7@users.noreply.github.com>
2026-04-28 08:48:22 +0800

    Update Pinduoduo_Filter.js

--- src-repo/QuantumultX/script/Pinduoduo_Filter.js
+++ src-repo/QuantumultX/script/Pinduoduo_Filter.js
@@ -1,15 +1,6 @@
-const url = $request.url;
 const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
-
-if (ua && ua.includes("com.xunmeng.pinduoduo")) {
-    const ipDirectRegex = /^http:\/\/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[[0-9a-fA-F:]+\])\/d(\d)?/;
-    
-    if (ipDirectRegex.test(url)) {
-        console.log("🚫 拦截拼多多 IP 直连: " + url);
-        $done({ policy: "REJECT" });
-    } else {
-        $done({});
-    }
+if (ua && ua.indexOf('com.xunmeng.pinduoduo') !== -1) {
+    $done({ response: { status: 403, body: "" } });
 } else {
     $done({});
 }
