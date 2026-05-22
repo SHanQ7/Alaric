20f12c39f4793ee1682067bfb115ce026255aef2
Allen <144861909+SHanQ7@users.noreply.github.com>
2026-05-23 03:20:40 +0800

    Delete src-repo/QuantumultX/script/Pinduoduo_Filter.js

--- src-repo/QuantumultX/script/Pinduoduo_Filter.js
+++
@@ -1,6 +0,0 @@
-const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
-if (ua && ua.indexOf('com.xunmeng.pinduoduo') !== -1) {
-    $done({ response: { status: 403, body: "" } });
-} else {
-    $done({});
-}
